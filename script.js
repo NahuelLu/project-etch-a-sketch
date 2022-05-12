394793993e3b014c06009bd187dbc667d7aa9de4
NahuelLu <nahuellucana1999@gmail.com>
2022-05-12 17:42:54 -0300

    Add button to create divs

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-"use strict"
 function createDivsInside(number){
-    const gridContainer = document.querySelector(".grid-container");
     for(let i=0; i < number*number;i++){
         let newDiv= document.createElement("div");
         let sizeSquare=600/number; //Change 600 to divparent's width.
@@ -8,16 +6,30 @@ function createDivsInside(number){
         newDiv.style.width=sizeSquare.toString() + "px";
         gridContainer.appendChild(newDiv);
     }
-}
-createDivsInside(16);
-let squares=document.querySelectorAll(".square");
-squares.forEach(square => {
+    let squares=document.querySelectorAll(".square");
+    squares.forEach(square => {
     square.addEventListener("mouseover",putBackgroundColor);
     square.addEventListener("mouseout",removeBackgroundColor);
 });
+}
+function showPromptToUser(){
+    let numberOfDivsForSide= prompt("Please put some number:");
+    if(numberOfDivsForSide>100){
+        alert("Sorry please put some number less to 100");
+        showPromptToUser();
+    }else{
+        gridContainer.replaceChildren();
+        createDivsInside(numberOfDivsForSide);
+    }
+}
+
 function putBackgroundColor(){
     this.classList.add("changeColor");
 }
 function removeBackgroundColor(){
     this.classList.remove("changeColor");
 }
+const gridContainer = document.querySelector(".grid-container");
+createDivsInside(16);
+let button =document.querySelector(".button");
+button.addEventListener("click",showPromptToUser);
